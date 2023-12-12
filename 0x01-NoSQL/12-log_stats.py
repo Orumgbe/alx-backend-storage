bc3e8e74037678354cd4f1653a5f2c0f2bbe3358
Orumgbe <orumgbeugbekile@gmail.com>
2023-12-12 09:02:30 -0800

    Access Nginx log in mongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-"""Script that provides stats about Nginx logs in MongoDB"""
-
+"""a Python script that provides some stats about Nginx logs"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
+    '''provides some stats about Nginx logs'''
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    print("{} logs".format(logs_collection.estimated_document_count()))
+    col = client.logs.nginx
+    print("{} logs".format(col.estimated_document_count()))
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = logs_collection.count_documents({'method': method})
+        count = col.count_documents({'method': method})
         print("\tmethod {}: {}".format(method, count))
-    status_get = logs_collection.count_documents({'method': 'GET',
-                                                 'path': "/status"})
+    status_get = col.count_documents({'method': 'GET', 'path': "/status"})
     print("{} status check".format(status_get))
